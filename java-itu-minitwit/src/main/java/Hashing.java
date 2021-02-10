bfb43deb1d8879e416c716f9968a1e13198e41e5
= <=>
2021-02-10 11:22:29 +0100

    Implemented initial Hashing class
    Implemented private method for getting hashstring and public methods for generating password hash, checking password against hash and generating hexidecimal representation of hash

--- java-itu-minitwit/src/main/java/Hashing.java
+++ java-itu-minitwit/src/main/java/Hashing.java
@@ -1,11 +1,38 @@
-import org.apache.commons.lang3.NotImplementedException;
+import java.security.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class Hashing {
-    public static String generate_password_hash(String password) {
-        throw new NotImplementedException("Not implemented");
+
+    private static String generate_hash_string(String input) {
+        String hashString = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            hashString = Base64.getEncoder().encodeToString(hashBytes);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hashString;
     }
 
-    public static boolean check_password_hash(String password1, String password2) {
-        throw new NotImplementedException("Not implemented");
+    public static String generate_password_hash(String password) {
+        return generate_hash_string(password);
+    }
+
+    public static boolean check_password_hash(String passwordHash, String password) {
+        return generate_password_hash(password).equals(passwordHash);
+    }
+
+    public static String generate_hash_hex(String input) {
+        String hashString = generate_hash_string(input);
+
+        StringBuffer sb = new StringBuffer();
+        char[] hashChars = hashString.toCharArray();
+        for(int i = 0; i < hashChars.length; i++) {
+            String hexString = Integer.toHexString(hashChars[i]);
+            sb.append(hexString);
+        }
+        return sb.toString();
     }
 }
