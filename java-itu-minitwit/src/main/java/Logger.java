50da1761cac39b5a768e8270360c816e33135731
join <join@itu.dk>
2021-03-17 10:11:11 +0100

    fixed logging complaints -tests

--- java-itu-minitwit/src/main/java/Logger.java
+++ java-itu-minitwit/src/main/java/Logger.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
-import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -13,32 +12,27 @@ public class Logger {
     private static final long LOGGING_PERIOD_SECONDS = 15;
     private static OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
 
-    static final Gauge cpuLoad = Gauge.build()
+    private static final Gauge cpuLoad = Gauge.build()
             .name("CPU_load").help("CPU load on server.").register();
-    static final Counter requests = Counter.build()
+    private static final Counter requests = Counter.build()
             .name("requests_total").help("Total requests.").register();
-    static final Gauge users = Gauge.build()
+    private static final Gauge users = Gauge.build()
             .name("users_total").help("Total amount of users.").register();
-    static final Gauge followers = Gauge.build()
+    private static final Gauge followers = Gauge.build()
             .name("followers_total").help("Total amount of followers.").register();
-    static final Gauge messages = Gauge.build()
+    private static final Gauge messages = Gauge.build()
             .name("messages_total").help("Total amount of messages.").register();
-    static final Gauge avgFollowers = Gauge.build()
+    private static final Gauge avgFollowers = Gauge.build()
             .name("followers_average").help("Average amount of followers per user.").register();
-    static final Gauge responseTimePublicTimeLine = Gauge.build()
+    private static final Gauge responseTimePublicTimeLine = Gauge.build()
             .name("response_time_publicTIme").help("response time for public timeLine.").register();
 
 
-    public static void StartLogging() throws IOException {
-        System.out.println("Started logging information");
-        StartSchedules();
-    }
-
-    public static void StartSchedules() {
+    public static void startSchedules() {
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
         executorService.scheduleAtFixedRate(Logger::LogUserInformation, 1, LOGGING_PERIOD_SECONDS , TimeUnit.SECONDS);
     }
-    private static void LogUserInformation() {
+    private static void logUserInformation() {
         processCpuLoad();
         processUsers();
         processFollowers();
@@ -76,7 +70,7 @@ public class Logger {
         }
     }
 
-    public static void LogResponseTimeFrontPage(long rt) {
+    public static void logResponseTimeFrontPage(long rt) {
         responseTimePublicTimeLine.set(rt);
     }
 }
