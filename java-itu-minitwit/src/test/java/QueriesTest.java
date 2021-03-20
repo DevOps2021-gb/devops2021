5aee5f45bcd45db2e792f5435b9c5096b4353937
JesperFalkenberg <jesperfalkenberg@outlook.dk>
2021-03-20 14:50:37 +0100

    removed parens from assert call in tests

--- java-itu-minitwit/src/test/java/QueriesTest.java
+++ java-itu-minitwit/src/test/java/QueriesTest.java
@@ -1,7 +1,7 @@
+
 import RoP.Result;
 import RoP.Success;
 import org.junit.jupiter.api.Test;
-import spark.utils.Assert;
 
 import static spark.Spark.stop;
 
@@ -47,8 +47,8 @@ class QueriesTest {
         register(username, password, password2, email);
         login(username, password);
         var id = Queries.getUserId(username);
-        assert (id.isSuccess());
-        assert (id.get() == userId);
+        assert id.isSuccess();
+        assert id.get() == userId;
         userId++;
         return id;
     }
@@ -61,7 +61,7 @@ class QueriesTest {
     //Records a message
     void add_message(String text, int loggedInUserId) {
         var rs = Queries.addMessage(text, loggedInUserId);
-        assert (rs.get());
+        assert rs.get();
         try {
             Thread.sleep(100);      //hotfix: added to ensure order of messages
         } catch (InterruptedException e) { }
@@ -73,41 +73,41 @@ class QueriesTest {
     @Test
     void test_register(){
         Logger.processUsers();
-        assert ((int) Logger.getUsers() == 0);
-        assert (Queries.getCountUsers().get() == 0);
+        assert (int) Logger.getUsers() == 0;
+        assert Queries.getCountUsers().get() == 0;
 
         var error = register("user1", "q123", null, null);
-        assert (error.isSuccess() && error.get().equals("OK"));
-        assert (Queries.getCountUsers().get() == 1);
+        assert error.isSuccess() && error.get().equals("OK");
+        assert Queries.getCountUsers().get() == 1;
         Logger.processUsers();
-        assert ((int) Logger.getUsers() == 1);
+        assert (int) Logger.getUsers() == 1;
 
         error = register("user1", "q123", null, null);
-        assert (!error.isSuccess() && error.getFailureMessage().equals("The username is already taken"));
+        assert !error.isSuccess() && error.getFailureMessage().equals("The username is already taken");
         error = register("", "q123", null, null);
-        assert (!error.isSuccess() && error.getFailureMessage().equals("You have to enter a username"));
+        assert !error.isSuccess() && error.getFailureMessage().equals("You have to enter a username");
         error = register("user2", "", null, null);
-        assert (!error.isSuccess() && error.getFailureMessage().equals("You have to enter a password"));
+        assert !error.isSuccess() && error.getFailureMessage().equals("You have to enter a password");
         error = register("user2", "1", "2", null);
-        assert (!error.isSuccess() && error.getFailureMessage().equals("The two passwords do not match"));
+        assert !error.isSuccess() && error.getFailureMessage().equals("The two passwords do not match");
         error = register("user2", "1", null, "bad email");
-        assert (!error.isSuccess() && error.getFailureMessage().equals("You have to enter a valid email address"));
-        assert (Queries.getCountUsers().get() == 1);
+        assert !error.isSuccess() && error.getFailureMessage().equals("You have to enter a valid email address");
+        assert Queries.getCountUsers().get() == 1;
         Logger.processUsers();
-        assert ((int) Logger.getUsers() == 1);
+        assert (int) Logger.getUsers() == 1;
     }
 
     @Test
     void test_login_logout() {
         var result = register_and_login("user1", "default");
-        assert (result.isSuccess() && result.get());
+        assert result.isSuccess() && result.get();
         result = logout();
-        assert (result.isSuccess() && result.get()); //TODO will always succeed as is now
+        assert result.isSuccess() && result.get(); //TODO will always succeed as is now
         result = login("user2", "wrongpassword");
         var msg = result.getFailureMessage();
-        assert (!result.isSuccess() && result.getFailureMessage().equals("Invalid username"));
+        assert !result.isSuccess() && result.getFailureMessage().equals("Invalid username");
         result = login("user1", "wrongpassword");
-        assert (!result.isSuccess() && result.getFailureMessage().equals("Invalid password"));
+        assert !result.isSuccess() && result.getFailureMessage().equals("Invalid password");
     }
 
     @Test
@@ -118,51 +118,51 @@ class QueriesTest {
         add_message(text1, id1.get());
         add_message(text2, id1.get());
         var rs = Queries.publicTimeline();
-        assert (rs.isSuccess());
+        assert rs.isSuccess();
         var tweet1 = rs.get().get(1);
         var tweet2 = rs.get().get(0);
-        assert (tweet1.getEmail().equals("foo@example.com"));
-        assert (tweet1.getUsername().equals("foo"));
-        assert (tweet1.getText().equals(text1));
+        assert tweet1.getEmail().equals("foo@example.com");
+        assert tweet1.getUsername().equals("foo");
+        assert tweet1.getText().equals(text1);
 
-        assert (tweet2.getEmail().equals("foo@example.com"));
-        assert (tweet2.getUsername().equals("foo"));
-        assert (tweet2.getText().equals(text2));//todo store as: "&lt;test message 2&gt;"
+        assert tweet2.getEmail().equals("foo@example.com");
+        assert tweet2.getUsername().equals("foo");
+        assert tweet2.getText().equals(text2);//todo store as: "&lt;test message 2&gt;"
     }
 
     @Test
     void test_getPersonalTweetsById() {
-        assert (Queries.getCountMessages().get() == 0);
+        assert Queries.getCountMessages().get() == 0;
         Logger.processMessages();
-        assert ((int) Logger.getMessages() == 0);
+        assert (int) Logger.getMessages() == 0;
 
         var id1 = register_login_getID("foo", "default", null, null);
         add_message("the message by foo", id1.get());
-        assert (Queries.getCountMessages().get() == 1);
+        assert Queries.getCountMessages().get() == 1;
         Logger.processMessages();
-        assert ((int) Logger.getMessages() == 1);
+        assert (int) Logger.getMessages() == 1;
 
 
         logout();
         var id2 = register_login_getID("bar","default", null, null);
         add_message("the message by bar", id2.get());
-        assert (Queries.getCountMessages().get() == 2);
+        assert Queries.getCountMessages().get() == 2;
         Logger.processMessages();
-        assert ((int) Logger.getMessages() == 2);
+        assert (int) Logger.getMessages() == 2;
 
         var rs = Queries.getPersonalTweetsById(id1.get());
-        assert (rs.isSuccess());
+        assert rs.isSuccess();
         var tweet1 = rs.get().get(0);
-        assert (tweet1.getEmail().equals("foo@example.com"));
-        assert (tweet1.getUsername().equals("foo"));
-        assert (tweet1.getText()).equals("the message by foo");
+        assert tweet1.getEmail().equals("foo@example.com");
+        assert tweet1.getUsername().equals("foo");
+        assert tweet1.getText().equals("the message by foo");
 
         rs = Queries.getPersonalTweetsById(id2.get());
-        assert (rs.isSuccess());
+        assert rs.isSuccess();
         var tweet2 = rs.get().get(0);
-        assert (tweet2.getEmail().equals("bar@example.com"));
-        assert (tweet2.getUsername().equals("bar"));
-        assert (tweet2.getText().equals("the message by bar"));
+        assert tweet2.getEmail().equals("bar@example.com");
+        assert tweet2.getUsername().equals("bar");
+        assert tweet2.getText().equals("the message by bar");
     }
 
     @Test
@@ -174,18 +174,18 @@ class QueriesTest {
         add_message("the message by bar", id2.get());
 
         var rs = Queries.getTweetsByUsername("foo");
-        assert (rs.isSuccess());
+        assert rs.isSuccess();
         var tweet1 = rs.get().get(0);
-        assert (tweet1.getEmail().equals("foo@example.com"));
-        assert (tweet1.getUsername().equals("foo"));
-        assert (tweet1.getText().equals("the message by foo"));
+        assert tweet1.getEmail().equals("foo@example.com");
+        assert tweet1.getUsername().equals("foo");
+        assert tweet1.getText().equals("the message by foo");
 
         rs = Queries.getTweetsByUsername("bar");
-        assert (rs.isSuccess());
+        assert rs.isSuccess();
         var tweet2 = rs.get().get(0);
-        assert (tweet2.getEmail().equals("bar@example.com"));
-        assert (tweet2.getUsername().equals("bar"));
-        assert (tweet2.getText().equals("the message by bar"));
+        assert tweet2.getEmail().equals("bar@example.com");
+        assert tweet2.getUsername().equals("bar");
+        assert tweet2.getText().equals("the message by bar");
     }
 
     @Test
@@ -195,16 +195,15 @@ class QueriesTest {
         var id1_rs = Queries.getUserId("foo");
         var user1_2 = Queries.getUserById(id1.get());
 
-        assert (id1.get().equals(id1_rs.get()));
-        assert (user1_1.get().id == id1.get());
-        assert (user1_1.get().getUsername().equals("foo"));
-        assert (user1_1.get().getPwHash().equals(Hashing.generatePasswordHash("default")));
-        assert (user1_1.get().getEmail().equals("myEmail@itu.dk"));
-        assert (
-                user1_1.get().id == user1_2.get().id &&
-                user1_1.get().getUsername().equals(user1_2.get().getUsername()) &&
-                user1_1.get().getPwHash().equals(user1_2.get().getPwHash()) &&
-                user1_1.get().getEmail().equals(user1_2.get().getEmail()));
+        assert id1.get().equals(id1_rs.get());
+        assert user1_1.get().id == id1.get();
+        assert user1_1.get().getUsername().equals("foo");
+        assert user1_1.get().getPwHash().equals(Hashing.generatePasswordHash("default"));
+        assert user1_1.get().getEmail().equals("myEmail@itu.dk");
+        assert user1_1.get().id == user1_2.get().id
+                        && user1_1.get().getUsername().equals(user1_2.get().getUsername())
+                        && user1_1.get().getPwHash().equals(user1_2.get().getPwHash())
+                        && user1_1.get().getEmail().equals(user1_2.get().getEmail());
     }
 
 
@@ -215,28 +214,28 @@ class QueriesTest {
         var id3 = register_login_getID("brian","q123", null, null);
 
 
-        assert (Queries.getCountFollowers().get() == 0);
+        assert Queries.getCountFollowers().get() == 0;
         Logger.processFollowers();
-        assert ((int) Logger.getFollowers() == 0);
+        assert (int) Logger.getFollowers() == 0;
 
         var rs1 = Queries.followUser(id1.get(), "bar");
-        assert (rs1.isSuccess());
-        assert (Queries.isFollowing(id1.get(), id2.get()).get());
-        assert (Queries.getCountFollowers().get() == 1);
+        assert rs1.isSuccess();
+        assert Queries.isFollowing(id1.get(), id2.get()).get();
+        assert Queries.getCountFollowers().get() == 1;
         Logger.processFollowers();
-        assert ((int) Logger.getFollowers() == 1);
+        assert (int) Logger.getFollowers() == 1;
 
         var rs2 = Queries.followUser(id1.get(), "brian");
-        assert (rs2.isSuccess());
-        assert (Queries.isFollowing(id1.get(), id3.get()).get());
-        assert (Queries.getCountFollowers().get() == 2);
+        assert rs2.isSuccess();
+        assert Queries.isFollowing(id1.get(), id3.get()).get();
+        assert Queries.getCountFollowers().get() == 2;
         Logger.processFollowers();
-        assert ((int) Logger.getFollowers() == 2);
+        assert (int) Logger.getFollowers() == 2;
 
         var rs = Queries.getFollowing(id1.get());
-        assert (rs.isSuccess());
-        assert (rs.get().get(0).getUsername().equals("bar"));
-        assert (rs.get().get(1).getUsername().equals("brian"));
+        assert rs.isSuccess();
+        assert rs.get().get(0).getUsername().equals("bar");
+        assert rs.get().get(1).getUsername().equals("brian");
     }
 
     @Test
@@ -249,24 +248,24 @@ class QueriesTest {
         add_message("the message by bar", id2.get());
 
         var rs1 = Queries.followUser(id1.get(), "bar");
-        assert (rs1.isSuccess());
+        assert rs1.isSuccess();
         var rs2 = Queries.followUser(id1.get(), "brian");
-        assert (rs2.isSuccess());
+        assert rs2.isSuccess();
 
 
-        assert (Queries.isFollowing(id1.get(), id2.get()).get());
-        assert (Queries.isFollowing(id1.get(), id3.get()).get());
+        assert Queries.isFollowing(id1.get(), id2.get()).get();
+        assert Queries.isFollowing(id1.get(), id3.get()).get();
 
         var rsUnfollow1 = Queries.unfollowUser(id1.get(), "bar");
-        assert (rsUnfollow1.isSuccess());
-        assert (!Queries.isFollowing(id1.get(), id2.get()).get());
+        assert rsUnfollow1.isSuccess();
+        assert !Queries.isFollowing(id1.get(), id2.get()).get();
         var rsUnfollow2 = Queries.unfollowUser(id1.get(), "brian");
-        assert (rsUnfollow2.isSuccess());
-        assert (!Queries.isFollowing(id1.get(), id3.get()).get());
+        assert rsUnfollow2.isSuccess();
+        assert !Queries.isFollowing(id1.get(), id3.get()).get();
 
         var rs = Queries.getFollowing(id1.get());
-        assert (rs.isSuccess());
-        assert (rs.get().size()==0);
+        assert rs.isSuccess();
+        assert rs.get().size()==0;
     }
 
 
@@ -282,20 +281,20 @@ class QueriesTest {
 
 
         var rTweets = Queries.getPersonalTweetsById(id1.get());
-        assert (rTweets.isSuccess());
-        assert (rTweets.get().size()==1);
-        assert (rTweets.get().get(0).getUsername().equals("foo")   && rTweets.get().get(0).getText().equals("the message by foo"));
+        assert rTweets.isSuccess();
+        assert rTweets.get().size()==1;
+        assert rTweets.get().get(0).getUsername().equals("foo")   && rTweets.get().get(0).getText().equals("the message by foo");
 
         var rs1 = Queries.followUser(id1.get(), "bar");
         var rs2 = Queries.followUser(id1.get(), "brian");
-        assert (rs1.isSuccess() && rs2.isSuccess());
+        assert rs1.isSuccess() && rs2.isSuccess();
 
         rTweets = Queries.getPersonalTweetsById(id1.get());
-        assert (rTweets.isSuccess());
-        assert (rTweets.get().get(0).getUsername().equals("brian") && rTweets.get().get(0).getText().equals("the message by Biran v2"));
-        assert (rTweets.get().get(1).getUsername().equals("brian") && rTweets.get().get(1).getText().equals("the message by Biran v1"));
-        assert (rTweets.get().get(2).getUsername().equals("bar")   && rTweets.get().get(2).getText().equals("the message by bar"));
-        assert (rTweets.get().get(3).getUsername().equals("foo")   && rTweets.get().get(3).getText().equals("the message by foo"));
+        assert rTweets.isSuccess();
+        assert rTweets.get().get(0).getUsername().equals("brian") && rTweets.get().get(0).getText().equals("the message by Biran v2");
+        assert rTweets.get().get(1).getUsername().equals("brian") && rTweets.get().get(1).getText().equals("the message by Biran v1");
+        assert rTweets.get().get(2).getUsername().equals("bar")   && rTweets.get().get(2).getText().equals("the message by bar");
+        assert rTweets.get().get(3).getUsername().equals("foo")   && rTweets.get().get(3).getText().equals("the message by foo");
     }
 
     //todo: queryLogin
--- java-itu-minitwit/src/test/java/RoPTest.java
+++ java-itu-minitwit/src/test/java/RoPTest.java
@@ -1,3 +1,4 @@
+
 import RoP.Failure;
 import RoP.Result;
 import RoP.Success;
@@ -8,21 +9,22 @@ class RoPTest {
     @Test
     void test_Success() {
         Result<Integer> v = new Success<>(2);
-        assert (v.isSuccess());
-        assert (v.get() == 2);
+        assert v.isSuccess();
+        assert v.get() == 2;
     }
+
     @Test
     void test_Failure() {
         var v1 = new Failure<Integer>(new IndexOutOfBoundsException("test"));
-        assert (!v1.isSuccess());
-        assert (v1.getException().getClass() == IndexOutOfBoundsException.class);
-        assert (v1.getFailureMessage().equals("test"));
+        assert !v1.isSuccess();
+        assert v1.getException().getClass() == IndexOutOfBoundsException.class;
+        assert v1.getFailureMessage().equals("test");
 
 
         var v2 = new Failure<Integer>("test");
-        assert (!v2.isSuccess());
-        assert (v2.getException().getClass() == Exception.class);
-        assert (v2.getFailureMessage().equals("test"));
+        assert !v2.isSuccess();
+        assert v2.getException().getClass() == Exception.class;
+        assert v2.getFailureMessage().equals("test");
     }
 
 }
