83391ee52fca98750d89f6458fd40abaf2db078c
jonasIshoejNielsen <join@itu.dk>
2021-03-20 18:30:05 +0100

    fixed Logger

--- java-itu-minitwit/src/main/java/logic/Logger.java
+++ java-itu-minitwit/src/main/java/logic/Logger.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiFunction;
-
 import io.prometheus.client.Counter;
 import io.prometheus.client.Gauge;
 import spark.Request;
@@ -17,26 +16,27 @@ import spark.Response;
 public class Logger {
     private static final long LOGGING_PERIOD_SECONDS = 15;
     private static OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-
     private static final Gauge cpuLoad = Gauge.build()
-            .name("CPU_load").help("CPU load on server.").register();
+        .name("CPU_load").help("CPU load on server.").register();
     private static final Counter requests = Counter.build()
-            .name("requests_total").help("Total requests.").register();
+        .name("requests_total").help("Total requests.").register();
     private static final Gauge users = Gauge.build()
-            .name("users_total").help("Total amount of users.").register();
+        .name("users_total").help("Total amount of users.").register();
     private static final Gauge followers = Gauge.build()
-            .name("followers_total").help("Total amount of followers.").register();
+        .name("followers_total").help("Total amount of followers.").register();
     private static final Gauge messages = Gauge.build()
-            .name("messages_total").help("Total amount of messages.").register();
-    private static final Map<String, Gauge> responseTimeEndPoints=new HashMap<>();
+        .name("messages_total").help("Total amount of messages.").register();
+    private static final Map<String, Gauge> responseTimeEndPoints = new HashMap<>();
 
     private Logger() {
     }
 
-    private static void setEndpoints(String[] endpoints, String namePrefix, String helpPrefix){
-        for(String key: endpoints){
+    private static void setEndpoints(String[] endpoints, String namePrefix, String helpPrefix) {
+        for (String key: endpoints){
+            String endPoint     = new StringBuilder(namePrefix).append(key.replace('/', '_')).toString();
+            String helpString   = new StringBuilder(helpPrefix).append(key).toString();
             Gauge gauge = Gauge.build()
-                    .name(namePrefix+key.replace('/', '_')).help(helpPrefix+key).register();
+                .name(endPoint).help(helpString).register();
             responseTimeEndPoints.put(key, gauge);
         }
     }
@@ -47,7 +47,7 @@ public class Logger {
     }
     public static void startSchedules() {
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        executorService.scheduleAtFixedRate(Logger::logUserInformation, 1, LOGGING_PERIOD_SECONDS , TimeUnit.SECONDS);
+        executorService.scheduleAtFixedRate(Logger::logUserInformation, 1, LOGGING_PERIOD_SECONDS, TimeUnit.SECONDS);
     }
     private static void logUserInformation() {
         processCpuLoad();
@@ -59,19 +59,19 @@ public class Logger {
     public static void processRequest() {
         requests.inc();
     }
-    public static void processCpuLoad(){
+    public static void processCpuLoad() {
         var cpuLoadLastMinute   = operatingSystemMXBean.getSystemLoadAverage() / operatingSystemMXBean.getAvailableProcessors();
         cpuLoad.set(cpuLoadLastMinute);
     }
-    public static void processUsers(){
+    public static void processUsers() {
         long numberOfUsers = Queries.getCountUsers().get();
         users.set(numberOfUsers);
     }
-    public static void processFollowers(){
+    public static void processFollowers() {
         long numberOfFollowers = Queries.getCountFollowers().get();
         followers.set(numberOfFollowers);
     }
-    public static void processMessages(){
+    public static void processMessages() {
         long numberOfMessages = Queries.getCountMessages().get();
         messages.set(numberOfMessages);
     }
@@ -90,10 +90,10 @@ public class Logger {
         return followers.get();
     }
 
-    public static Object benchMarkEndpoint(String endPointName, BiFunction<Request, Response, Object> endpoint, Request req, Response res){
+    public static Object benchMarkEndpoint(String endPointName, BiFunction<Request, Response, Object> endpoint, Request req, Response res) {
         var startTime = System.currentTimeMillis();
         Object result = endpoint.apply(req, res);
-        Logger.logResponseTimeEndpoint(endPointName,System.currentTimeMillis() - startTime);
+        Logger.logResponseTimeEndpoint(endPointName, System.currentTimeMillis() - startTime);
         return result;
     }
 
