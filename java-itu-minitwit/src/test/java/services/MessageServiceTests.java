c243bb54b317b2466404421f0e99c92a979b916f
frem <frem@itu.dk>
2021-03-30 10:56:48 +0200

    moved some unit tests from MessageServiceTests to UserServiceTests

--- java-itu-minitwit/src/test/java/services/MessageServiceTests.java
+++ java-itu-minitwit/src/test/java/services/MessageServiceTests.java
@@ -2,53 +2,14 @@ package services;
 
 import org.junit.jupiter.api.Assertions;
 import persistence.MessageRepository;
-import persistence.UserRepository;
 import org.junit.jupiter.api.Test;
 import testUtilities.DatabaseTestBase;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
 class MessageServiceTests extends DatabaseTestBase {
-    @Test
-    void validateUserCredentialsGivenAlreadyExistingUserReturnsUserAlreadyExists() {
-        var result = UserRepository.addUser("user1", "test@test.dk", "q123");
-        Assertions.assertTrue(result.isSuccess());
-        Assertions.assertEquals("OK", result.get());
-        Assertions.assertEquals((long) UserRepository.countUsers().get(), 1);
-        LogService.processUsers();
-        Assertions.assertEquals(LogService.getUsers(), 1);
-
-        result = UserService.validateUserCredentials("user1", "test@test.dk", "q123", "q123");
-        Assertions.assertFalse(result.isSuccess());
-        Assertions.assertEquals("The username is already taken",result.getFailureMessage());
-    }
-
-    @Test
-    void validateUserCredentialsGivenNoUsernameReturnsMissingUsername() {
-        var error = UserService.validateUserCredentials("", "q123", null, null);
-        Assertions.assertFalse(error.isSuccess());
-        Assertions.assertEquals("You have to enter a username", error.getFailureMessage());
-    }
-
-    @Test
-    void validateUserCredentialsGivenNoPasswordReturnsMissingPassword() {
-        var error = UserService.validateUserCredentials("user2", "test@test.dk", "", null);
-        Assertions.assertFalse(error.isSuccess());
-        Assertions.assertEquals("You have to enter a password", error.getFailureMessage());
-    }
-
-    @Test
-    void validateUserCredentialsGivenNonMatchingPasswordsReturnsNonMatchingPasswords() {
-        var error = UserService.validateUserCredentials("user2", "test@test.dk", "2", "1");
-        Assertions.assertFalse(error.isSuccess());
-        Assertions.assertEquals("The two passwords do not match", error.getFailureMessage());
-    }
-
-    @Test
-    void validateUserCredentialsGivenInvalidEmailReturnsInvalidEmail() {
-        var error = UserService.validateUserCredentials("user2", "1", null, "bad email");
-        Assertions.assertFalse(error.isSuccess());
-        Assertions.assertEquals("You have to enter a valid email address", error.getFailureMessage());
-    }
-
     @Test
     void testLoginLogout() {
         var result = this.registerAndLogin("user1", "default");
@@ -82,5 +43,26 @@ class MessageServiceTests extends DatabaseTestBase {
         Assertions.assertEquals( "foo", tweet2.getUsername());
         //todo store as: "&lt;test message 2&gt;"
         Assertions.assertEquals(text2, tweet2.getText());
+    }
+
+    @Test
+    void tweetsFromListOfHashMapGivenHashmapReturnsTweets() {
+        HashMap hm = new HashMap();
+        hm.put("email", "test@test.dk");
+        hm.put("username", "test");
+        hm.put("text", "body");
+        hm.put("pubDate", new Date().getTime());
+        var maps = new ArrayList<HashMap>();
+        maps.add(hm);
+
+        var actual = MessageService.tweetsFromListOfHashMap(maps);
+
+        Assertions.assertEquals(1, actual.size());
+        var tweet = actual.get(0);
+        Assertions.assertEquals(tweet.getEmail(), "test@test.dk");
+        Assertions.assertEquals(tweet.getUsername(), "test");
+        Assertions.assertEquals(tweet.getText(), "body");
+        Assertions.assertNotNull(tweet.getPubDate());
+        Assertions.assertNotNull(tweet.getProfilePic());
     }
 }
---
+++ java-itu-minitwit/src/test/java/services/UserServiceTests.java
@@ -0,0 +1,49 @@
+package services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import persistence.UserRepository;
+
+public class UserServiceTests {
+    @Test
+    void validateUserCredentialsGivenAlreadyExistingUserReturnsUserAlreadyExists() {
+        var result = UserRepository.addUser("user1", "test@test.dk", "q123");
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertEquals("OK", result.get());
+        Assertions.assertEquals((long) UserRepository.countUsers().get(), 1);
+        LogService.processUsers();
+        Assertions.assertEquals(LogService.getUsers(), 1);
+
+        result = UserService.validateUserCredentials("user1", "test@test.dk", "q123", "q123");
+        Assertions.assertFalse(result.isSuccess());
+        Assertions.assertEquals("The username is already taken",result.getFailureMessage());
+    }
+
+    @Test
+    void validateUserCredentialsGivenNoUsernameReturnsMissingUsername() {
+        var error = UserService.validateUserCredentials("", "q123", null, null);
+        Assertions.assertFalse(error.isSuccess());
+        Assertions.assertEquals("You have to enter a username", error.getFailureMessage());
+    }
+
+    @Test
+    void validateUserCredentialsGivenNoPasswordReturnsMissingPassword() {
+        var error = UserService.validateUserCredentials("user2", "test@test.dk", "", null);
+        Assertions.assertFalse(error.isSuccess());
+        Assertions.assertEquals("You have to enter a password", error.getFailureMessage());
+    }
+
+    @Test
+    void validateUserCredentialsGivenNonMatchingPasswordsReturnsNonMatchingPasswords() {
+        var error = UserService.validateUserCredentials("user2", "test@test.dk", "2", "1");
+        Assertions.assertFalse(error.isSuccess());
+        Assertions.assertEquals("The two passwords do not match", error.getFailureMessage());
+    }
+
+    @Test
+    void validateUserCredentialsGivenInvalidEmailReturnsInvalidEmail() {
+        var error = UserService.validateUserCredentials("user2", "1", null, "bad email");
+        Assertions.assertFalse(error.isSuccess());
+        Assertions.assertEquals("You have to enter a valid email address", error.getFailureMessage());
+    }
+}
