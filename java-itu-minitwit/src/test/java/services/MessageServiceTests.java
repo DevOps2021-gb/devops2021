86473627fe6fb1d7de20251bdf4de75b9247b325
frem <frem@itu.dk>
2021-04-28 18:57:03 +0200

    added messageservicetests

--- java-itu-minitwit/src/test/java/services/MessageServiceTests.java
+++ java-itu-minitwit/src/test/java/services/MessageServiceTests.java
@@ -1,6 +1,9 @@
 package services;
 
+import errorhandling.Failure;
+import errorhandling.Success;
 import model.dto.DTO;
+import model.dto.MessagesPerUserDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import repository.IMessageRepository;
@@ -10,6 +13,8 @@ import repository.UserRepository;
 import utilities.*;
 import view.IPresentationController;
 import view.PresentationController;
+
+import java.util.ArrayList;
 
 import static org.mockito.Mockito.*;
 
@@ -27,12 +32,72 @@ class MessageServiceTests {
     }
 
     @Test
-    public void getMessagesGivenUnauthorizedDtoReturns403() {
+    public void getMessagesGivenUnauthorizedDtoReturnsUnauthorized() {
         var dto = new DTO();
         dto.authorization = "unauthorized";
 
+        var expected = "You are not authorized to use this resource!";
+
+        when(responses.notFromSimulatorResponse()).thenReturn(expected);
+
         String actual = (String) GetService().getMessages(dto);
 
-        Assertions.assertEquals("You are not authorized to use this resource!", actual);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMessagesGivenAuthorizedDtoReturnsTweets() {
+        var dto = new DTO();
+        dto.authorization = "authorized";
+
+        when(messageRepository.publicTimeline()).thenReturn(new Success<>(new ArrayList<>()));
+        when(requests.isFromSimulator(dto.authorization)).thenReturn(true);
+
+        GetService().getMessages(dto);
+
+        verify(jsonFormatter, times(1)).tweetsToJSONResponse(any());
+    }
+
+    @Test
+    public void messagesPerUserGivenRequestNotFromSimulatorReturnsUnauthorized() {
+        var dto = new MessagesPerUserDTO();
+        dto.authorization = "unauthorized";
+
+        var expected = "You are not authorized to use this resource!";
+
+        when(responses.notFromSimulatorResponse()).thenReturn(expected);
+
+        String actual = (String) GetService().messagesPerUser(dto);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void messagesPerUserGivenExistingUserReturnsTweets() {
+        var dto = new MessagesPerUserDTO();
+        dto.authorization = "authorized";
+        dto.username = "abc";
+
+        when(requests.isFromSimulator(dto.authorization)).thenReturn(true);
+        when(userRepository.getUserId(dto.username)).thenReturn(new Success<>(1));
+        when(messageRepository.getTweetsByUsername(any(String.class))).thenReturn(new Success<>(new ArrayList<>()));
+
+        GetService().messagesPerUser(dto);
+
+        verify(jsonFormatter, times(1)).tweetsToJSONResponse(any());
+    }
+
+    @Test
+    public void messagesPerUserGivenNonExistingUserReturnsNotFound() {
+        var dto = new MessagesPerUserDTO();
+        dto.authorization = "authorized";
+        dto.username = "abc";
+
+        when(requests.isFromSimulator(dto.authorization)).thenReturn(true);
+        when(userRepository.getUserId(dto.username)).thenReturn(new Failure<>(new Exception()));
+
+        GetService().messagesPerUser(dto);
+
+        verify(responses, times(1)).respond404();
     }
 }
